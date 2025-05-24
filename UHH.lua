063dceecf6e88276a871a8b970587010a457b43c
The1nfplayer <bachtuyet2023nc@gmail.com>
2025-05-24 08:30:02 +0700

    Create UHH.lua

---
+++ UHH.lua
@@ -0,0 +1,7 @@
+
+-- Setup Window
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("UHH | Made by 1nf", win_theme)
+
+-- Player tab
+local Player_T = Window:NewTab("Player")
