4b420550ff6b31af274bde62758eea57030dee3a
The1nfplayer <bachtuyet2023nc@gmail.com>
2025-05-24 11:13:08 +0700

    Update UHH.lua
    fixed nothing

--- UHH.lua
+++ UHH.lua
@@ -27,7 +27,7 @@ S_Health_Player:NewToggle("God mode", "A toggle that makes you immortal (not at 
             if player.Character and player.Character:FindFirstChild("Humanoid") then
                 player.Character.Humanoid.Health = player.Character.Humanoid.MaxHealth
             end
-            task.wait(0.1) -- mượt và đỡ lỗi hơn
+            task.wait(0.1)
         end
     end)
 end)
